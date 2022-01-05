f703c9c54ec6b987224c78b151a2617b86d7afc4
Cameron Henige <godmodecameron@gmail.com>
2022-01-05 01:33:47 -0500

    Initial Commit

---
+++ TestMap/TestMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMapApp.swift
+//  TestMap
+//
+//  Created by Hannah Krolewski on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
